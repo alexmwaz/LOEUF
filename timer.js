4fc90f42559963197af755629b6bc96940da75e1
Kaitlyn <kscason@users.noreply.github.com>
2015-01-31 02:37:10 -0800

    Update timer.js

--- timer.js
+++ timer.js
@@ -2,50 +2,24 @@
 
 var EggType = prompt( How do you wish to cook your egg? ('h' for HARDBOILED and 's' for SOFTBOILED));
 
-var Egg
+//creates egg variable
+var Egg = 
 {
     m_type: EggType;
     
     if( m_type === "s" )
-        m_time: 3*60;
+        m_timeVar: 120000; //2 minutes
     else if( m_type === "h" )
-        m_time: 5*60;
+        m_timeVar: 480000; //8 minutes
     else
-        m_time: 0;
+        m_timeVar: 0; //cut out
 };
-<script src="/js/countdown.js"></script>
 
-var EggTime = function()
+var start=Date.now(),r=document.getElementById('r');
+(function f()
 {
-    clock_t startTime = clock(); //Start timer
-    var secondsPassed;
-    var secondsToDelay = m_time;
-    var timeToPrint = 0;
-    console.log("You must wait a total of: " + secondsToDelay/60 + " minutes!!!!!");
-    bool flag = true;
-    while(flag)
-    {
-        secondsPassed = (clock() - startTime) / CLOCKS_PER_SEC;
-        
-        if(secondsPassed >= secondsToDelay)
-        {
-            console.log("0" + secondsToDelay/60 + ":00 minutes have passed. Your time is up!!");
-            flag = false;
-        }
-        else if( secondsPassed != timeToPrint )
-        {
-            var minutes = secondsPassed/60;
-            var seconds = secondsPassed - minutes*60;
-            
-            if( secondsPassed > 9 && secondsPassed < 60 )
-                console.log( "00:" +secondsPassed );
-            else if( minutes > 0 && seconds < 10 )
-                console.log("0" + minutes +":0" + seconds);
-            else if( minutes > 0 && seconds >= 10 )
-                console.log("0" + minutes + ":" + seconds);
-            else
-                console.log("00:0" + secondsPassed);
-            timeToPrint++;
-        }
-    }
-};
+    var diff = Date.now() - start, ns = ((( Egg.m_timeVar- diff) / 1000) >> 0), m = (ns / 60) >> 0, s = ns - m*60;
+    r.textContent="Registration closes in "+m+':'+((''+s).length>1?'':'0')+s+' minutes';
+ if(diff>(Egg.m_timeVar)){start=Date.now()}
+ setTimeout(f,1000);
+})();
